bfe6c4bb876965bb8616bed2136588ec9f85d0ac
troman29 <mc-monstr@yandex.ru>
2024-07-14 01:53:59 +0300

    Add mainnet giver address

--- config.ts
+++ config.ts
@@ -6,4 +6,4 @@ export const IS_TESTNET = process.env.IS_TESTNET === '1';
 
 export const givers = IS_TESTNET
     ? [{ address: 'EQDe1EaGTLsqY5K_lQcqViPXxBg6ANjlZ3v4PxzaQkolOqW8' }]
-    : [{ address: '' }];
+    : [{ address: 'EQAzwqEwOx1EJTLLlrEhLNSW3eGUgQYgHOjMtvtO89HpOfHA' }];
