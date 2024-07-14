90b7778dcd1bce09b13c16c5ad1847854c6b6b55
troman29 <mc-monstr@yandex.ru>
2024-07-14 14:23:04 +0300

    Remove GPU files

--- start_gpu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-npm install
-
-
-while true; do
-  node send_universal_gpu.js --api tonapi --bin ./pow-miner-cuda
-  sleep 1;
-done;
--- start_gpu_tonhub.bat
+++
@@ -1,8 +0,0 @@
-
-call ".\npm_install.bat"
-
-:_minerstart
-node send_universal_gpu.js --api tonhub
-goto _minerstart
-
-pause
