e7d03ba3e2f72f5ae777290dab428972b8316baf
troman29 <mc-monstr@yandex.ru>
2024-08-08 16:00:25 +0300

    Add GIVER env var

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config({ path: './config.txt' });
 exports.IS_TESTNET = process.env.IS_TESTNET === '1';
 exports.givers = exports.IS_TESTNET
-    ? [{ address: 'EQDe1EaGTLsqY5K_lQcqViPXxBg6ANjlZ3v4PxzaQkolOqW8' }]
-    : [{ address: 'EQCg1vPyYQxySttDcTtgT-ZtfgzOsODnu1-giGHjnc-FYG0E' }];
+    ? [{ address: process.env.GIVER || 'EQDe1EaGTLsqY5K_lQcqViPXxBg6ANjlZ3v4PxzaQkolOqW8' }]
+    : [{ address: process.env.GIVER || 'EQCg1vPyYQxySttDcTtgT-ZtfgzOsODnu1-giGHjnc-FYG0E' }];
--- config.ts
+++ config.ts
@@ -5,5 +5,5 @@ dotenv.config({ path: './config.txt' });
 export const IS_TESTNET = process.env.IS_TESTNET === '1';
 
 export const givers = IS_TESTNET
-    ? [{ address: 'EQDe1EaGTLsqY5K_lQcqViPXxBg6ANjlZ3v4PxzaQkolOqW8' }]
-    : [{ address: 'EQCg1vPyYQxySttDcTtgT-ZtfgzOsODnu1-giGHjnc-FYG0E' }];
+    ? [{ address: process.env.GIVER || 'EQDe1EaGTLsqY5K_lQcqViPXxBg6ANjlZ3v4PxzaQkolOqW8' }]
+    : [{ address: process.env.GIVER || 'EQCg1vPyYQxySttDcTtgT-ZtfgzOsODnu1-giGHjnc-FYG0E' }];
